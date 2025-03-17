4f8bc792c6dee659d5110e22699760290f0836f8
Nozdormuu <zv.danila@yandex.ru>
2025-03-17 13:12:24 +0300

    DZTVP-8: was corrected main.c with float mode

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // Определяем режим работы
+    int isFloatMode = 0;
     if (strcmp(argv[1], "float") == 0) {
         isFloatMode = 1;
     } else if (strcmp(argv[1], "int") != 0) {
@@ -21,100 +21,127 @@ int main(int argc, char* argv[])
     }
 
     char* expression = argv[2];
-
-    // Check for valid characters and initial conditions
-    int lastWasOperator = 1; // Start with true to allow a number at the start
+    int lastWasOperator = 1;
     int openBrackets = 0;
 
+    // Validation loop
     for (char* p = expression; *p; p++) {
         if (isspace(*p))
-            continue; // Skip whitespace
+            continue;
 
         if (isdigit(*p)) {
-            lastWasOperator = 0; // We found a number
-            while (isdigit(*p)) {
+            lastWasOperator = 0;
+            while (isdigit(*p))
                 p++;
-            }
-            if (*p == '.') { // If we encounter a decimal point, it's an error
-                fprintf(stderr, "Error: Invalid character - floating point number detected\n");
+            if (*p == '.' && !isFloatMode) {
+                fprintf(stderr, "Error: Floating point in integer mode\n");
                 return 1;
             }
-            p--; // Adjust for the loop increment
+            p--;
         } else if (*p == '(') {
             openBrackets++;
-            lastWasOperator = 1; // Reset, as '(' is not an operator
+            lastWasOperator = 1;
         } else if (*p == ')') {
-            if (lastWasOperator) { // If the last was an operator, it's an error
-                fprintf(stderr, "Error: Unexpected closing parenthesis\n");
+            if (openBrackets <= 0) {
+                fprintf(stderr, "Error: Mismatched parentheses\n");
                 return 1;
             }
             openBrackets--;
+            lastWasOperator = 0;
         } else if (strchr("+-*/", *p)) {
-            if (lastWasOperator) { // If the last was also an operator, it's an error
-                fprintf(stderr, "Error: Two consecutive operators\n");
+            if (lastWasOperator) {
+                fprintf(stderr, "Error: Consecutive operators\n");
                 return 1;
             }
-            lastWasOperator = 1; // We found an operator
+            lastWasOperator = 1;
         } else {
             fprintf(stderr, "Error: Invalid character\n");
-            return 1; // Return code not equal to 0
+            return 1;
         }
     }
 
-    if (openBrackets != 0) {
-        fprintf(stderr, "Error: Mismatched parentheses\n");
+    if (openBrackets != 0 || lastWasOperator) {
+        fprintf(stderr, "Error: Invalid expression format\n");
         return 1;
     }
 
-    if (lastWasOperator) { // If the expression ends with an operator, it's an error
-        fprintf(stderr, "Error: Expression cannot end with an operator\n");
-        return 1;
-    }
-
-    IntStack intValues;
-    IntStack intOps;
+    // Initialize stacks
+    IntStack intValues, intOps;
     FloatStack floatValues;
-
     initIntStack(&intValues);
     initIntStack(&intOps);
     initFloatStack(&floatValues);
 
+    // Main processing loop
     for (char* p = expression; *p; p++) {
         if (isspace(*p))
-            continue; // Skip whitespace
+            continue;
 
         if (isdigit(*p)) {
             if (isFloatMode) {
-                fprintf(stderr, "Error: Invalid character - floating point number detected\n");
-                return 1; // We can't have floats in float mode
+                float num = 0;
+                int decimal = 0;
+                float divisor = 1.0000f;
+
+                while (isdigit(*p) || *p == '.') {
+                    if (*p == '.') {
+                        decimal = 1;
+                    } else {
+                        if (decimal) {
+                            divisor *= 10.0f;
+                            num = num + (*p - '0') / divisor;
+                        } else {
+                            num = num * 10 + (*p - '0');
+                        }
+                    }
+                    p++;
+                }
+                p--;
+                floatPush(&floatValues, num);
+            } else {
+                int num = 0;
+                while (isdigit(*p)) {
+                    num = num * 10 + (*p - '0');
+                    p++;
+                }
+                p--;
+                intPush(&intValues, num);
             }
-            int num = 0;
-            while (isdigit(*p)) {
-                num = num * 10 + (*p - '0');
-                p++;
-            }
-            intPush(&intValues, num);
-            p--; // Adjust for the loop increment
         } else if (*p == '(') {
             intPush(&intOps, *p);
         } else if (*p == ')') {
-            while (!isIntEmpty(&intOps) && intOps.items[intOps.top] != '(') {
-                evaluateInt(&intValues, &intOps);
+            while (!isIntEmpty(&intOps) && intPeek(&intOps) != '(') {
+                if (isFloatMode)
+                    evaluateFloat(&floatValues, &intOps);
+                else
+                    evaluateInt(&intValues, &intOps);
             }
-            intPop(&intOps); // Remove '(' from stack
+            intPop(&intOps); // Remove '('
         } else { // Operator
             while (!isIntEmpty(&intOps) && precedence(intPeek(&intOps)) >= precedence(*p)) {
-                evaluateInt(&intValues, &intOps);
+                if (isFloatMode)
+                    evaluateFloat(&floatValues, &intOps);
+                else
+                    evaluateInt(&intValues, &intOps);
             }
             intPush(&intOps, *p);
         }
     }
 
+    // Final evaluation
     while (!isIntEmpty(&intOps)) {
-        evaluateInt(&intValues, &intOps);
+        if (isFloatMode)
+            evaluateFloat(&floatValues, &intOps);
+        else
+            evaluateInt(&intValues, &intOps);
     }
 
-    printf("%d\n", intPop(&intValues));
+    // Output result
+    if (isFloatMode) {
+        printf("%.4f\n", floatPeek(&floatValues));
+    } else {
+        printf("%d\n", intPeek(&intValues));
+    }
 
     return 0;
 }
